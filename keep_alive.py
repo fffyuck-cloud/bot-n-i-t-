c59a5a8f19232dd1c64f14b4b8ebad1c01f7aa98
fffyuck-cloud <fffyuck@gmail.com>
2026-08-14 11:42:42 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Tà Ma Đạo Giáo Bot đang sống!"
+    return "Bot is online and running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
